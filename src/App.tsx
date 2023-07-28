7b8c88a4b62ac2fa31f7a758fa22eb5cb23c870e
Pietro Castelli <pietrocast03@gmail.com>
2023-07-28 20:46:55 +0200

    [ADD] delete item

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,15 @@ function App() {
   function TodoList(e: FormEvent){
     e.preventDefault()
     setTodoList((current)=>{
-      return [...current,{id:1,text:newItem,confirm: true}]
+      return [...current,{id:Number(crypto.randomUUID()),text:newItem,confirm: true}]
+    })
+  }
+
+  function deleteTodo(id : number){
+    setTodoList((current)=>{
+        return current.filter((item)=>{
+          item.id !== id
+        })
     })
   }
   return (
@@ -25,14 +33,13 @@ function App() {
       <input type="text" placeholder='write' value={newItem} onChange={e => setNewItem(e.target.value)}/>
       <button>Add</button>
     </form>
-     
       {todoList.map((item) =>{
         return<div className='todo-list'>
-          <label >
+          <label key={item.id}>
             <input type='checkbox' checked={item.confirm}/>
             {item.text}
           </label>
-          <button >Delete</button>
+          <button onClick={()=>deleteTodo(item.id)}>Delete</button>
 
         </div>
       })}
